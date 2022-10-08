d77bdb900075e92de88595e7b6a621484d7a4897
unknown <nazm.s@northeastern.edu>
2022-10-07 20:16:20 -0400

    added employee

---
+++ src/HRApp/Employee.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HRApp;
+
+import java.awt.image.BufferedImage;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Saqib Nazm
+ */
+public class Employee {
+	private String name;
+	private String empId;
+	private int age;
+	private String gender;
+	private LocalDate startDate;
+	private String level;
+	private String teamInfo;
+	private String position;
+	private String phone;
+	private String email;
+	private BufferedImage photo;
+	
+	public Employee() {
+	}
+	
+	public Employee(String name, String empId, int age, String gender, LocalDate startDate, String level, String teamInfo, String position, String phone, String email, BufferedImage photo) {
+		this.name = name;
+		this.empId = empId;
+		this.age = age;
+		this.gender = gender;
+		this.startDate = startDate;
+		this.level = level;
+		this.teamInfo = teamInfo;
+		this.position = position;
+		this.phone = phone;
+		this.email = email;
+		this.photo = photo;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmpId() {
+		return empId;
+	}
+	
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+	
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+	
+	public String getLevel() {
+		return level;
+	}
+	
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	
+	public String getTeamInfo() {
+		return teamInfo;
+	}
+	
+	public void setTeamInfo(String teamInfo) {
+		this.teamInfo = teamInfo;
+	}
+	
+	public String getPosition() {
+		return position;
+	}
+	
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public BufferedImage getPhoto() {
+		return photo;
+	}
+	
+	public void setPhoto(BufferedImage photo) {
+		this.photo = photo;
+	}
+}
+
